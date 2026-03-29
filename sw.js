9b0bc154199c1495ae2484a23ae1ac636d04e5de
manuelbermudo2804-tech <manuelbermudo2804@gmail.com>
2026-03-29 14:09:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", function(e) {
-  console.log("Service Worker instalado");
+self.addEventListener('install', function(e) {
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', function(event) {
+  // necesario para que sea PWA válida
 });
